ffc5bf64c310a110ebd3c9b13dba2d57987c9362
Mayank Gupta <85787633+Mayank9511@users.noreply.github.com>
2022-10-09 23:50:43 +0530

    Added Prime Factors

---
+++ C++/primeFactor.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+void primeFac(int n)
+{
+    int spf[100]={0};
+
+    for(int i=2;i<=n;i++){
+        spf[i]=i;
+    }
+
+    for(int i=2 ; i<=n; i++)
+    {
+        if(spf[i]==i)
+        {
+            for(int j=i*i;j<=n;j+=i){
+                if(spf[j]==j)
+                {
+                spf[j]=i;
+                }
+            }
+        }
+    }
+
+    while (n!=1)
+    {
+        cout<<spf[n]<<" ";
+
+        n = n/spf[n];
+
+        /* code */
+    }
+    
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    primeFac(n);
+}
