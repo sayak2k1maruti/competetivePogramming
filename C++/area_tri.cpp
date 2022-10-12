4c0f669857a76cf3c3fd87008dfc18d95b818852
Sreya <100512237+BotSonu@users.noreply.github.com>
2022-10-12 14:34:48 +0530

    Area and   Perimeter   of   a Triangle in C++
    Write a program to print the area and perimeter of a triangle having sides of 3, 4 and 5 units by creating a class named 'Triangle' with the constructor having the three sides as its parameters.

--- C++/area_tri.cpp
+++ C++/area_tri.cpp
@@ -1,3 +1,7 @@
+/*  Write a program to print the area and perimeter of a triangle having sides of 3, 4 and 5 units 
+    by creating a class named 'Triangle'with the constructor having the three sides as its parameters. */
+    
+    
 #include<iostream>
 #include<math.h>
 
